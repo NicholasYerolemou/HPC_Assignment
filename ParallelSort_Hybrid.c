5d4196ac81d48cba50858d3b504f4c6d7acc5c3d
NicholasYerolemou <yerolemou.nicholas@gmail.com>
2023-05-17 15:51:53 +0200

    fixed all errors so that it selects the correct pivots

--- ParallelSort_Hybrid.c
+++ ParallelSort_Hybrid.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <stdbool.h>
 
 double hybrid_psrs_sort(int *arr, int n, int p);
 // int *hybrid_merge_sort(int *arr, int size);
@@ -60,11 +61,16 @@ double hybrid_psrs_sort(int *arr, int n, int p)
     size = (n_per + p - 1) / p; // size of each threads sub-array
     // printf("size %lld\n", size);
     rsize = (size + p - 1) / p;
-    sample_size = p * (p - 1); // the number of samples to be taken by this node
+    sample_size = (p * (p - 1)) / numNodes; // the number of samples to be taken by this node
+    if (sample_size == 0)
+    {
+        sample_size = 1;
+    }
+    // printf("num samples per node %d\n", sample_size);
 
     loc_a_ptrs = malloc(p * sizeof(int *));
-    sample = malloc(sample_size * sizeof(int)); // this holds the samples from this node
-    // printf("Rank:%d sample size%d\n", rank, sizeof(sample) / sizeof(int));
+    sample = malloc(sample_size * sizeof(int)); // this holds the samples from this node is of size sample_size
+
     partition_borders = malloc(p * (p + 1) * sizeof(int));
     bucket_sizes = malloc(p * sizeof(int));
     result_positions = malloc(p * sizeof(int));
@@ -78,100 +84,119 @@ double hybrid_psrs_sort(int *arr, int n, int p)
 
 #pragma omp parallel // distribute this node's part of the arr amongst threads
     {
-        int i, j, max, thread_num, start, end, loc_size, offset, this_result_size;
-        int *loc_a, *this_result, *current_a;
-
+        int i, j, thread_num, start, end, loc_size, offset, this_result_size;
+        int *per_thread_subarray, *this_result, *current_a;
         thread_num = omp_get_thread_num();
         start = thread_num * size; // multiples of 32, starting index of where this threads values are placed
         end = start + size - 1;
 
         if (end >= n_per)
             end = n_per - 1;
-        loc_size = (end - start + 1); // size of each threads subarray = 32// this changes to 26 for thread 7 which is correct as that is how many elements are left of the 250
+        loc_size = (end - start + 1); // size of each threads subarray = 32 // this changes to 26 for thread 7 which is correct as that is how many elements are left of the 250
         end = end % size;             // now end is just telling us how much we need to add to start to get to the next start value = 124
 
+        // per_thread_subarray is per thread
+        per_thread_subarray = malloc(loc_size * sizeof(int));           // we are taking samples per thread
+        memcpy(per_thread_subarray, a + start, loc_size * sizeof(int)); // puttig them in per_thread_subarray
+        loc_a_ptrs[thread_num] = per_thread_subarray;
+
+        sortll(per_thread_subarray, loc_size);
+
+        offset = fmax(((thread_num * (p - 1)) - 1) / numNodes, thread_num);
         // if (rank == 0)
         // {
-        //     printf("thread: %d start %d\n", thread_num, start);
-        //     printf("thread: %d end %d\n", thread_num, end);
-        //     printf("loc_size %d\n", loc_size);
+        //     printf("thread num %d offset %d\n", thread_num, offset);
         // }
+        int range = (sample_size + p - 1) / p;
 
-        // loc_a is per thread
-        loc_a = malloc(loc_size * sizeof(int));           // we are taking samples per thread
-        memcpy(loc_a, a + start, loc_size * sizeof(int)); // puttig them in loc_a // this is correct
-        loc_a_ptrs[thread_num] = loc_a;
-
-        // for (int i = 0; i < loc_size; ++i)
+        // printf("%d\n", range);
+        // if (thread_num == 31)
         // {
-        //     printf("Rank:%d: %d\n", rank, loc_a[i]);
+        //     printf("%d", loc_size);
         // }
-
-        sortll(loc_a, loc_size);
-
-        offset = thread_num * (p - 1) - 1;
-
-        for (int i = 1; i < p; i++) // moving the samples we collected into the main sample array, it holds the samples from all threads
+        for (int i = 0; i < range; i++) // moving the samples we collected into the main sample array, it holds the samples from all threads
         {
+            // rsize is size of each threads subarray
+            // if (i * rsize <= end)
+            // {
+            rsize = loc_size / range;
 
-            if (i * rsize <= end)
+            if ((((i + 1) * rsize) - 1) >= 0 && (((i + 1) * rsize) - 1) <= (loc_size - 1))
             {
-                // printf("Top: Rank:%d: %lld\n", rank, loc_a[i * rsize - 1]);
-                sample[offset + i] = loc_a[i * rsize - 1];
+                sample[offset + i] = per_thread_subarray[((i + 1) * rsize) - 1];
             }
             else
             {
-                // printf("bottom: Rank:%d: %lld\n", rank, loc_a[end]);
-                sample[offset + i] = loc_a[end];
+
+                // printf("Thread:%d val:%d\n", thread_num, per_thread_subarray[loc_size - 1]);
+
+                sample[offset + i] = per_thread_subarray[loc_size - 1]; // come back to this !!!!!!!!!!!!!!
             }
         }
     }
 
 #pragma omp barrier
 
-    // they each take the correct number of sampples but the actual values are not rigtht
-    for (int i = 0; i < sample_size; ++i)
+    // if (rank == 0)
+    // {
+    //     // printf("Thread:%d/n", omp_get_thread_num());
+    //     for (int i = 0; i < sample_size; ++i) // check the sample values
+    //     {
+    //         printf("i %d: Rank:%d: %d\n", i, rank, sample[i]);
+    //     }
+    // }
+
+    // int *samples = (int *)calloc(size, sizeof(int));
+
+    int *samples_all; // holds the samples from all the nodes
+    if (rank == 0)
     {
-        printf("Rank:%d: %d\n", rank, sample[i]);
+        samples_all = (int *)calloc((numNodes * sample_size), sizeof(int));
+        assert(samples_all != NULL);
     }
 
-    //     // int *samples = (int *)calloc(size, sizeof(int));
+    // we are actually taking p samples per node. if nodes = 4 we are taking 4 times as many samples as we should
 
-    //     int *samples_all;
-    //     if (rank == 0)
+#pragma omp single
+    {
+
+        MPI_Barrier(MPI_COMM_WORLD);
+        // gather the samples data from all processes to the root process
+        MPI_Gather(sample, sample_size, MPI_INT, samples_all, sample_size, MPI_INT, 0,
+                   MPI_COMM_WORLD);
+
+        // if (rank == 0)
+        // {
+        //     for (int i = 0; i < numNodes * sample_size; ++i)
+        //     {
+        //         printf("%d\n", samples_all[i]);
+        //     }
+        // }
+
+        if (rank == 0) // sort the samples and select pivots
+        {
+            // printf("There are %li sampels", sizeof(samples_all) / sizeof(int));
+            merge_sort(samples_all, numNodes * sample_size); // we need to work out sample size, because we are actually taking more samples than we should
+            for (int i = 0; i < p - 1; i++)
+            {
+                printf("%d\n", samples_all[i * p + p / 2]);
+                pivots[i] = samples_all[i * p + p / 2]; // select the pivots from the sample array
+            }
+        }
+    }
+
+#pragma omp barrier
+
+    // if (rank == 0)
+    // {
+    //     for (int i = 0; i < p - 1; ++i)
     //     {
-    //         samples_all = (int *)calloc((size * size), sizeof(int));
-    //         assert(samples_all != NULL);
+    //         printf("Pivots: %d\n", pivots[i]);
     //     }
+    // }
 
-    // this needs to hold the samples from each node
-    // we are actually taking p samples per node. if nodes = 2 we are taking twice as many samples as we should
-
-    // #pragma omp single
-    //     {
-
-    //         MPI_Barrier(MPI_COMM_WORLD);
-    //         // gather the samples data from all processes to the root process
-    //         MPI_Gather(sample, sizeMPI, MPI_INT, samples_all, sizeMPI, MPI_INT, 0,
-    //                    MPI_COMM_WORLD);
-
-    //         if (rank == 0)
-    //         {
-    //             printf("There are %li sampels", sizeof(samples_all) / sizeof(int));
-    //             // merge_sort(samples_all, sample_size); // we need to work out sample size, because we are actually taking more samples than we should
-    //             // for (int i = 0; i < p - 1; i++)
-    //             // {
-    //             //     pivots[i] = sample[i * p + p / 2]; // select the pivots from the sample array
-    //             // }
-    //         }
-    //     }
-
-    // #pragma omp barrier
-
-    //     for (int i = 0; i < p; ++i)
-    //     {
-    //         printf("Pivots: %lld %d", pivots[i], rank);
-    //     }
+    MPI_Bcast(pivots, (p - 1), MPI_INT, 0, MPI_COMM_WORLD);
+    // redistribute pivots
 
     // qsort(a, n_per, sizeof(int), cmp);//sorting of threads on this node
 
